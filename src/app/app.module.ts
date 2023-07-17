7c865f69c6e14e06f1e126cb1414f92061eff6b4
Barbosa Thaíssa <thathamaria03@gmail.com>
2023-07-17 00:14:49 -0300

    feat: Component card post I

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { CommentFormComponent } from './comments/comment-form/comment-form.compo
 import { CommentListComponent } from './comments/comment-list/comment-list.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { PostCardComponent } from './layouts/post-card/post-card.component';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { AboutUsComponent } from './pages/about-us/about-us.component';
     SubscriptionFormComponent,
     CommentFormComponent,
     CommentListComponent,
-    AboutUsComponent
+    AboutUsComponent,
+    PostCardComponent
   ],
   imports: [
     BrowserModule,
